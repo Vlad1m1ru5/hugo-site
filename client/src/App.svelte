5ee89a547e7713388100d10f0298d0e073d5bdaf
Kubantsev-VA <Kubantsev.V.A@sberbank.ru>
2020-08-19 08:37:51 +0300

    develop
    svelte select style

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,11 +1,41 @@
 <script>
+	import Select from 'svelte-select'
+
 	const greatings = 'HELLO FROM SVELTE'
+
+	const items = [
+		{ value: "smth", label: "Smth" },
+		{ value: "marko", label: "Marko" },
+		{ value: "uno", label: "Uno" },
+		{ value: "polo", label: "Polo" },
+		{ value: "foo", label: "Foo" },
+		{ value: "bar", label: "Bar" },
+		{ value: "baz", label: "Baz" }
+	]
 </script>
 
 <style>
-	.greatings {
-	  color: #bc8f8f;
+	:root {
+		--main-color: #bc8f8f;
+		--complement-color: #8fbc8f;
+	}
+	.site-search {
+		--placeholderColor: var(--main-color);
+		
+		--border: 1px solid var(--main-color);
+		--borderRadius: 10px;
+		--borderFocusColor: var(--complement-color);
+		
+		--itemIsActiveBG: var(--main-color);
+		--itemHoverBG: var(--complement-color);
+	}
+
+	.site-search__title--light {
+	  color: var(--main-color);
 	}
 </style>
 
-<h1 class='greatings'>{greatings}</h1>
+<section class='site-search'>
+	<h1 class='site-search__title site-search__title--light'>{greatings}</h1>
+	<Select {items} class='site-search__select' />
+</section>
