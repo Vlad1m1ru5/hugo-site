b0b093246225a79bad4397faba515aff016141bc
Kubantsev-VA <Kubantsev.V.A@sberbank.ru>
2020-08-20 07:44:36 +0300

    develop
    fix item select with undefined title

---
+++ client/src/async-select.svelte
@@ -0,0 +1,53 @@
+<script>
+  import Select from 'svelte-select'
+  
+  export let Item = undefined
+
+  const noOptionsMessage = 'Нет результатов'
+  
+	const placeholder = 'Начните вводить текст поиска'
+
+	const optionIdentifier = "id"
+
+	const getOptionLabel = (option) => option.title
+	
+	const getSelectionLabel = (option) => option.title
+	
+	const loadOptions = async (filterText) => {
+
+		const pattern = filterText.trim().toLocaleLowerCase()
+
+		const itemFilter = ({ title, contents, permalink }) => {
+
+			const isInTitileFilterText = title.toLocaleLowerCase().includes(pattern)
+			const isInContentsFilterText = contents.toLocaleLowerCase().includes(pattern)
+
+			const result = (
+				isInContentsFilterText ||
+				isInTitileFilterText
+			)
+
+			return result
+		}
+
+		try {
+			const response = await fetch('/index.json')
+			const items = await response.json()
+			
+			const result = items.filter(itemFilter)
+
+			return result
+		} catch (error) {
+			return []
+		}
+	}
+</script>
+
+<Select
+  {placeholder}
+  {loadOptions}
+  {getOptionLabel}
+  {noOptionsMessage}
+  {getSelectionLabel}
+  {Item}
+/>
